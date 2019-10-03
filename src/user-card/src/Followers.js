93755f056c102ea4ef93f2068255dcd993520392
CScori <amilanotte1366@gmail.com>
2019-10-03 10:03:55 -0500

    MVP no Styling

--- src/user-card/src/Followers.js
+++ src/user-card/src/Followers.js
@@ -2,12 +2,13 @@ import React from 'react'
 class Followers extends React.Component {
 
     render(props) {
-        return(
-            <div> 
-            <h3>{props.login}</h3>
-          <img src={props.avatar_url} alt='follower'/>
-          </div>
+        return (
+            <div>
+                <h3>{props.login}</h3>
+                <img src={props.avatar_url} alt='follower' />
+            </div>
         )
     }
 };
+
 export default Followers;
